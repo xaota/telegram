9f25d7fb0a6324d9574667ce6d829e7d71cd509e
xaota <1172619+xaota@users.noreply.github.com>
2020-02-09 03:03:28 +0500

    ui-tabs + ui-tab

--- component/form/emoji/form-emoji.js
+++ component/form/emoji/form-emoji.js
@@ -1,5 +1,10 @@
 import Component from '../../../script/Component.js';
 
+import UITab  from '../../ui/tab/ui-tab.js';
+import UITabs from '../../ui/tabs/ui-tabs.js';
+
+import IconEmoji from '../../icon/emoji/icon-emoji.js';
+
 const component = Component.meta(import.meta.url, 'form-emoji');
 const attributes = {
 
