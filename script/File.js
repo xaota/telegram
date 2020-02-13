b9aeb17ed82c48926bdce99d7e4fc04a4e61faa4
xaota <1172619+xaota@users.noreply.github.com>
2020-02-13 15:51:34 +0500

    fix

--- script/File.js
+++ script/File.js
@@ -1,40 +1,37 @@
 import {channel} from './DOM.js';
 
-export default class File {
-    async getFile(file) {
-        if (!file.id) return;
-        if (file.local.is_downloading_completed) return this.readFile(id);
+export class File {
+  async getFile(file, priority = 32) {
+    if (!file.id) return;
+    if (file.local.is_downloading_completed) return this.readFile(id);
 
-        return new Promise(resolve => { // сначала подпишемся
-            channel.on('updateFile', (loadFile) => {
-                if (loadFile.id === file.id && loadFile.local.is_downloading_completed) {
-                    this.readFile(loadFile.id)
-                        .then(blob => resolve(blob));
-                }
-            });
-            telegram.api('downloadFile', {
-                file_id: file.id,
-                priority: 32
-            });
-        });
-    }
+    return new Promise(resolve => { // сначала подпишемся
+      channel.on('updateFile', (loadFile) => {
+        if (loadFile.id !== file.id) return;
+        // TODO: также отобразить в канале промежуточный результат загрузки
+        if (loadFile.local.is_downloading_completed) return this.readFile(loadFile.id).then(blob => resolve(blob));
+      });
+      telegram.api('downloadFile', {file_id: file.id, priority});
+    });
+  }
 
-    async readFile(file_id) {
-        const file = await telegram.api('readFile', {file_id});
-        const blob = await new Promise(resolve => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file.data);
-            reader.onloadend = function() {
-                const base64data = reader.result;
-                resolve(base64data);
-            }
-        });
-        return blob;
-    }
+  async readFile(file_id) {
+    const file = await telegram.api('readFile', {file_id});
 
-    update(e) {
-        channel.send('updateFile', e.file);
-    }
+    const blob = await new Promise(resolve => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file.data); // с большими файлами не сработает
+      reader.onloadend = function() {
+        const base64data = reader.result;
+        resolve(base64data);
+      }
+    });
+    return blob;
+  }
+
+  update(e) {
+    channel.send('updateFile', e.file); //
+  }
 }
 
 export default new File();
