3df364d261402f7f617c3014446c331445156886
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2020-04-02 03:18:12 +0500

    Store base dialog data

--- state/dialogs/middleware/loadDialogsMiddleware.js
+++ state/dialogs/middleware/loadDialogsMiddleware.js
@@ -1,1 +1,35 @@
+import {LOAD_DIALOGS} from '../constants.js'
 
+const {from} = rxjs;
+const {filter, switchMap, catchError} = rxjs.operators;
+const {isActionOf} = store;
+const {method, construct} = zagram;
+
+
+function loadDialogsStream(connection, action) {
+  console.log('Load dialogs with action', action);
+  return R.pipe(
+    R.always({
+      limit: 20,
+      offset: 0,
+      offset_id: 0,
+      offset_peer: construct('inputPeerEmpty'),
+      hash: 0,
+    }),
+    R.partial(method, ['messages.getDialogs']),
+    x => from(connection.request(x)).pipe(catchError(R.of)),
+  )(action);
+}
+
+export default function loadDialogsMiddleware(action$, state$, connection) {
+  connection.addEventListener('statusChanged', (e) => {
+    if (e.status === 'AUTH_KEY_CREATED') {
+      const loadDialogs$  = action$.pipe(
+        filter(isActionOf(LOAD_DIALOGS)),
+        switchMap(R.partial(loadDialogsStream, [connection]))
+      );
+
+      loadDialogs$.subscribe(console.log);
+    }
+  });
+}
--- state/dialogs/reducer.test.js
+++ state/dialogs/reducer.test.js
@@ -4,7 +4,9 @@ import {
   DIALOGS_LOADED
 } from './constants.js';
 
-import reducer from './reducers.js';
+import reducer from './reducer.js';
+
+const {construct} = zagram;
 
 describe('dialogs', () => {
   describe('LOAD_DIALOGS', () => {
@@ -34,11 +36,70 @@ describe('dialogs', () => {
   describe('DIALOGS_LOADED', () => {
     const action = {
       type: DIALOGS_LOADED,
+      payload: [
+        construct(
+          'dialog',
+          {
+            pinned: false,
+            unread_mark: false,
+            peer: construct('peerUser', {'user_id': 77700}),
+            top_message: 208,
+            read_inbox_max_id: 199,
+            read_outbox_max_id: 14,
+            unread_count: 9,
+            unread_mentions_count: 0,
+            notify_settings: construct('peerNotifySettings', {}),
+          },
+        ),
+        construct(
+          'dialog',
+          {
+            pinned: false,
+            unread_mark: false,
+            peer: construct('peerUser', {user_id: 516572}),
+            top_message: 198,
+            read_inbox_max_id: 0,
+            read_outbox_max_id: 0,
+            unread_count: 0,
+            unread_mentions_count: 0,
+            notify_settings: construct('peerNotifySettings', {}),
+          },
+        ),
+      ],
     };
 
     const state = {dialogsLoading: true};
 
-    expect(reducer(state, action)).toEqual({});
+    expect(reducer(state, action)).toEqual({
+      dialogsOrder: [
+        'peer_user_77700',
+        'peer_user_516572',
+      ],
+      dialogs: {
+        'peer_user_77700': construct('dialog', {
+          pinned: false,
+          unread_mark: false,
+          peer: construct('peerUser', {'user_id': 77700}),
+          top_message: 208,
+          read_inbox_max_id: 199,
+          read_outbox_max_id: 14,
+          unread_count: 9,
+          unread_mentions_count: 0,
+          notify_settings: construct('peerNotifySettings', {}),
+        }),
+        'peer_user_516572': construct('dialog', {
+          pinned: false,
+          unread_mark: false,
+          peer: construct('peerUser', {user_id: 516572}),
+          top_message: 198,
+          read_inbox_max_id: 0,
+          read_outbox_max_id: 0,
+          unread_count: 0,
+          unread_mentions_count: 0,
+          notify_settings: construct('peerNotifySettings', {}),
+        }),
+      },
+    });
   });
 });
 
