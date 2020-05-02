219b26d89a68540ef4edd9fa2a40f4c507408557
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2020-05-02 11:46:52 +0500

    Handle all new messages

--- state/telegramUpdateMiddleware.js
+++ state/telegramUpdateMiddleware.js
@@ -1,24 +1,101 @@
 import {setChatList} from './chats/index.js';
 import {setUserList} from './users/index.js';
+import {prependMessage} from './dialogs/index.js';
+import {applyAll} from './utils.js';
+import {wrapAsObjWithKey} from '../script/helpers.js';
 
 const {of, fromEvent, merge} = rxjs;
-const {map, filter, tap, switchMap} = rxjs.operators;
+const {map, filter, tap, switchMap, withLatestFrom} = rxjs.operators;
 
-const {isObjectOf} = zagram;
+const {isObjectOf, construct} = zagram;
 
 const updateStateWithChats = R.pipe(R.prop('chats'), setChatList);
 const updateStateWithUsers = R.pipe(R.prop('users'), setUserList);
 
-const updateChatsUsersInfo = R.pipe(
-  R.of,
-  R.ap([
+const updateChatsUsersInfo = R.pipe(applyAll([
     updateStateWithChats,
     updateStateWithUsers
-  ])
+  ]));
+
+
+/**
+ * @param {[Object, Number]} param - tuple where first value is object of telegrams `updateShortMessage`
+ *                                   and second value is id of authorized user
+ * @returns {boolean} is out message or not
+ */
+const isOut = R.pathOr(false, [0, 'out']);
+
+/**
+ * @param {[Object, Number]} param - tuple where first value is object of telegrams `updateShortMessage`
+ *                                   and second value is id of authorized user
+ * @returns {Object} telegrams `peerUser` object
+ */
+const shortMessageToPeerUser = R.pipe(
+  R.nth(0),
+  R.pick(['user_id']),
+  R.partial(construct, ['peerUser'])
+);
+
+/**
+ * @param {[Object, Number]} param - tuple where first value is object of telegrams `updateShortMessage`
+ *                                   and second value is id of authorized user
+ * @returns {Object} telegrams `peerUser` object
+ */
+const userIdToPeerUser = R.pipe(
+  R.nth(1),
+  wrapAsObjWithKey('user_id'),
+  R.partial(construct, ['peerUser'])
+);
+
+/**
+ * @param {[Object, Number]} param - tuple where first value is object of telegrams `updateShortMessage`
+ *                                   and second value is id of authorized user
+ * @returns {Object} telegrams `peerUser` object
+ */
+const buildPeerUser = R.cond([
+  [isOut, shortMessageToPeerUser],
+  [R.T, userIdToPeerUser]
+]);
+
+/**
+ * @param {[Object, Number]} param - tuple where first value is object of telegrams `updateShortMessage`
+ *                                   and second value is id of authorized user
+ * @returns {Object} telegrams `peerUser` object
+ */
+const buildFromId = R.cond([
+  [isOut, R.nth(1)],
+  [R.T, R.path([0, 'user_id'])]
+]);
+
+/**
+ * @param {[Object, Number]} param - tuple where first value is object of telegrams `updateShortMessage`
+ *                                   and second value is id of authorized user
+ * @returns {Object} elegrams message object
+ */
+export const buildMessageFromUpdateShortMessage = R.pipe(
+  applyAll([
+    R.nth(0),
+    R.pipe(buildPeerUser, wrapAsObjWithKey('to_id')),
+    R.pipe(buildFromId, wrapAsObjWithKey('from_id'))
+  ]),
+  R.mergeAll,
+  R.partial(construct, ['message'])
 );
 
 
+export const buildMessageFromUpdateShortChatMessage = R.pipe(
+  applyAll([
+    R.identity,
+    R.pipe(R.partial(construct, ['peerChat']), wrapAsObjWithKey('to_id'))
+  ]),
+  R.mergeAll,
+  R.partial(construct, ['message'])
+);
+
 export default function telegramUpdateMiddleware(action$, state$, connection) {
+  const authUser$ = state$.pipe(map(R.path(['auth', 'user'])));
+  const authUserId$ = authUser$.pipe(map(R.prop('id')));
+
   const telegramUpdate$ = fromEvent(connection, 'telegramUpdate')
     .pipe(map(R.prop('detail')));
 
@@ -28,12 +105,16 @@ export default function telegramUpdateMiddleware(action$, state$, connection) {
       map(R.prop('update'))
     );
 
-  const updateShortMessage$ = telegramUpdate$.pipe(filter(isObjectOf('updateShortMessage')));
-  updateShortMessage$.subscribe(x => console.log('[UPDATE SHORT MESSAGE]:', x));
+  const updateShortMessage$ = telegramUpdate$.pipe(
+    filter(isObjectOf('updateShortMessage')),
+    withLatestFrom(authUserId$),
+    map(buildMessageFromUpdateShortMessage)
+  );
 
-  const updateShortChatMessage$ = telegramUpdate$.pipe(filter(isObjectOf('updateShortChatMessage')));
-
-  updateShortChatMessage$.subscribe(x => console.log('[UPDATE SHOT CHAT MESSAGE]', x));
+  const updateShortChatMessage$ = telegramUpdate$.pipe(
+    filter(isObjectOf('updateShortChatMessage')),
+    map(buildMessageFromUpdateShortChatMessage)
+  );
 
   const updates$ = telegramUpdate$.pipe(
     filter(isObjectOf('updates')),
@@ -45,12 +126,14 @@ export default function telegramUpdateMiddleware(action$, state$, connection) {
   );
 
   const update$ = merge(updates$, updateShort$);
-  const newMessage$ = update$.pipe(filter(R.anyPass([
+  const updateMessage$ = update$.pipe(
+    filter(R.anyPass([
       isObjectOf('updateNewChannelMessage'),
       isObjectOf('updateNewMessage')
-    ])));
+    ])),
+    map(R.prop('message'))
+  );
 
-  newMessage$.subscribe(x => {
-    console.log('New message:', R.prop('message', x));
-  });
+  const newMessage$ = merge(updateMessage$, updateShortMessage$, updateShortChatMessage$);
+  newMessage$.subscribe(prependMessage);
 }
